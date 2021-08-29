e9017314d2f8f6b28fc7976ebaafabe091fce99a
tta9799zzs <tta9799@gmail.com>
2021-08-30 01:39:56 +0700

    first commit

---
+++ user-service/src/main/java/com/snw/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.snw.user.VO;
+
+import com.snw.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
---
+++ user-service/src/main/java/com/snw/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.snw.user.controller;
+
+import com.snw.user.VO.ResponseTemplateVO;
+import com.snw.user.entity.User;
+import com.snw.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
+
+
+}
