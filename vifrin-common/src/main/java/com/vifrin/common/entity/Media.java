74c07b5d0bec5d44a449600da2a4e85fa928c703
trantuananh1 <tta9799@gmail.com>
2021-12-12 11:12:57 +0700

    hotfix media

---
+++ vifrin-common/src/main/java/com/vifrin/common/entity/Media.java
@@ -0,0 +1,87 @@
+package com.vifrin.common.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+/**
+ * @author: trantuananh1
+ * @since: Sun, 12/12/2021
+ **/
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "media")
+public class Media {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "url", length = 500)
+    private String url;
+
+    @Column(name = "mime")
+    private String mime;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "width")
+    private float width;
+
+    @Column(name = "height")
+    private float height;
+
+    @Column(name = "size")
+    private float size;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedBy
+    private Instant updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Media(String url, String name, String mime, float width, float height, float size) {
+        this.url = url;
+        this.mime = mime;
+        this.name = name;
+        this.width = width;
+        this.height = height;
+        this.size = size;
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Media{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                ", mime='" + mime + '\'' +
+                ", name='" + name + '\'' +
+                ", width=" + width +
+                ", height=" + height +
+                ", size=" + size +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
---
+++ vifrin-common/src/main/java/com/vifrin/common/repository/MediaRepository.java
@@ -0,0 +1,12 @@
+package com.vifrin.common.repository;
+
+import com.vifrin.common.entity.Media;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author: trantuananh1
+ * @since: Sun, 12/12/2021
+ **/
+
+public interface MediaRepository extends JpaRepository<Media, Long> {
+}
