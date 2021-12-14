07513b1b15937dce15864ecaeead37300a6656a2
Ta Quoc Hung <hungqtmta2021@gmail.com>
2021-12-14 19:36:47 +0700

    update config websocket

--- comment-service/src/main/java/com/vifrin/comment/config/WebSocketConfig.java
+++ comment-service/src/main/java/com/vifrin/comment/config/WebSocketConfig.java
@@ -1,17 +1,21 @@
 package com.vifrin.comment.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/vifrin/cmt").withSockJS();
+        registry.addEndpoint("/vifrin").setAllowedOrigins("*");
+        registry.addEndpoint("/vifrin").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
