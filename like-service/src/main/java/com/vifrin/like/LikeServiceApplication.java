5e1d19bd6f7d8ca2d923bb193888c07ec7ac9d41
trantuananh1 <tta9799@gmail.com>
2021-12-19 22:08:52 +0700

    update code

--- like-service/src/main/java/com/vifrin/like/LikeServiceApplication.java
+++ like-service/src/main/java/com/vifrin/like/LikeServiceApplication.java
@@ -1,6 +1,6 @@
 package com.vifrin.like;
 
-import com.vifrin.post.messaging.PostEventStream;
+import com.vifrin.like.messaging.PostEventStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
