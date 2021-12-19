f961538090b102f033f770bbf883cbe6588010d5
trantuananh1 <tta9799@gmail.com>
2021-12-19 23:14:25 +0700

    add *.sh to run all services

--- run-backend.sh
+++ run-backend.sh
@@ -1,2 +1,26 @@
 cd ./service-registry 
-./run.sh
+./run-backend.sh &
+cd ../cloud-config-server 
+./run-backend.sh &
+cd ../cloud-gateway
+./run-backend.sh &
+cd ../hystrix-dashboard 
+./run-backend.sh &
+cd ../auth-service 
+./run-backend.sh &
+cd ../user-service
+./run-backend.sh &
+cd ../media-service
+./run-backend.sh &
+cd ../post-service 
+./run-backend.sh &
+cd ../feed-service 
+./run-backend.sh &
+cd ../comment-service 
+./run-backend.sh &
+cd ../like-service 
+./run-backend.sh &
+cd ../destination-service 
+./run-backend.sh 
+
+
