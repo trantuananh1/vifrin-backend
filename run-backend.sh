7c807ed1864e0524893e1816860b1efb62b2a412
trantuananh1 <tta9799@gmail.com>
2021-12-20 14:40:35 +0700

    update run-backend.sh

--- run-backend.sh
+++ run-backend.sh
@@ -1,28 +1,41 @@
-cd ./service-registry 
+cd ./service-registry
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
-cd ../cloud-config-server 
+cd ../cloud-config-server
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
 cd ../cloud-gateway
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
-cd ../hystrix-dashboard 
+cd ../hystrix-dashboard
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
-cd ../auth-service 
+cd ../auth-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
 cd ../user-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
 cd ../media-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
-cd ../post-service 
+cd ../post-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
-cd ../feed-service 
+cd ../feed-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
-cd ../comment-service 
+cd ../comment-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
-cd ../like-service 
+cd ../like-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
-cd ../destination-service 
+cd ../destination-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh &
 cd ../search-service
+sudo chmod -R 777 ./run-backend.sh
 ./run-backend.sh
 
 
